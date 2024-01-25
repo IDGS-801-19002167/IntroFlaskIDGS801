821e21b1fca28444d4af5cb0c67bd6f8e125a113
GrAngelMorales <bmth.hreach@hotmail.com>
2024-01-24 22:15:12 -0600

    CCIRS_24012024_Forms
    Flask WTF

---
+++ forms.py
@@ -0,0 +1,7 @@
+from wtforms import Form
+from wtforms import StringField,TelField
+
+class UserForm(Form):
+    nombre = StringField("nombre")
+    email = StringField("email") #crear campo de validacion correcto
+    apaterno = TelField("apaterno")
